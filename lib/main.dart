bb1d0b6e0ccef5edb90d13eff79e1dad8bcb42d5
Marcio Pereira <marciojr.ads@gmail.com>
2021-06-10 23:35:41 -0300

    mudança de versão do 2.0 para o 1.22 flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,81 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Home(),
+    debugShowCheckedModeBanner: false,
   ));
 }
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  List _toDoList = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         backgroundColor: Colors.blueAccent,
         title: Text(
           "Lista de Tarefas",
           textAlign: TextAlign.center,
-          style: TextStyle(color: Colors.blue, fontSize: 15),
+          style: TextStyle(color: Colors.blue[900], fontSize: 20),
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: "Nova Tarefa",
+                      hintText: "Digite Aqui",
+                      hintStyle: TextStyle(color: Colors.blue),
+                      labelStyle: TextStyle(color: Colors.blueAccent),
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                  color: Colors.blueAccent,
+                  onPressed: () {},
+                  child: Text(" ADD "),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  Future<File> _getFile() async {
+    final Directory = await getApplicationSupportDirectory();
+    return File("${Directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
   }
 }
